ac9b45051a9b28b26caf1338777bd26e07a03d67
Ahmed Essam <ahmed.essam966@hotmail.com>
2023-10-13 05:30:34 +0300

    updated pocketbase exe

--- lib/Modules/Sales/Managers/Screens/sales_manager_screen.dart
+++ lib/Modules/Sales/Managers/Screens/sales_manager_screen.dart
@@ -43,7 +43,7 @@ class SalesManagerScreen extends StatelessWidget {
                     }),
                   ),
                 ),
-              )
+              ),
             ],
           ),
         ),
